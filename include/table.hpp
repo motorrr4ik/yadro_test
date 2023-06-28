11e4a0c12d7cd126ed1c70ff99a067a372d15572
motorrr4ik <nikita29042001@yandex.ru>
2023-06-28 18:16:54 +0300

    Definition finished

--- include/table.hpp
+++ include/table.hpp
@@ -1,18 +1,24 @@
 #ifndef TABlE
 #define TABLE
 
+#include<string>
+#include<sstream>
+#include<vector>
+
 class Table{
     private:
         int tableId;
         int costPerHour;
         int workingHours;
         bool ifBusy;
+        std::string startSession;
+        std::string endSession;
+        void countWorkingHours();
 
     public:
         Table(int tableId, int costPerHour, bool ifBusy);
-        void setStatus(bool status);
+        void setStatus(bool status, std::string const& time);
         bool isBusy();
-        void countWorkingHours();
         int countProfit();
 };
 
--- src/table.cpp
+++ src/table.cpp
@@ -2,15 +2,53 @@
 
 Table::Table(int tableId, int costPerHour, bool ifBusy):tableId(tableId), costPerHour(costPerHour), ifBusy(ifBusy), workingHours(0){};
 
-void Table::setStatus(bool status){
+void Table::setStatus(bool status, std::string const& time){
     ifBusy = status;
+    if(ifBusy){
+        startSession = time;
+    }else{
+        endSession = time;
+        countWorkingHours();
+    }
 }
 
 bool Table::isBusy(){
-    return ifBusy;
+    return isBusy;
 }
 
 int Table::countProfit(){
     return workingHours * costPerHour;
+}
+
+void Table::countWorkingHours(){
+    std::vector<std::string> startTimeDivided(2);
+    std::vector<std::string> endTimeDivided(2);
+    std::stringstream ssStart(startSession);
+    std::stringstream ssEnd(endSession);
+    std::string buffer;
+    int startHour = 0;
+    int endHour = 0;
+
+    while(std::getline(ssStart, buffer, ':')){
+        startTimeDivided.push_back(buffer);
+    }
+
+    while(std::getline(ssEnd, buffer, ':')){
+        endTimeDivided.push_back(buffer);
+    }
+
+    startHour = stoi(startTimeDivided[0]);
+    endHour = stoi(endTimeDivided[0]);
+
+    if(!startTimeDivided[1].empty()){
+        ++startHour;
+    }
+
+    if(!endTimeDivided[1].empty()){
+        ++endHour;
+    }
+
+    workingHours = workingHours + (startHour - endHour);
+
 }
 

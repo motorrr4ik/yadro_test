98abd05ed138881ef90b293bb791bd967ea8a182
motorrr4ik <nikita29042001@yandex.ru>
2023-06-29 16:54:44 +0300

    definition and declaration

--- include/command.hpp
+++ include/command.hpp
@@ -12,11 +12,18 @@ class Command{
     int tableId;
 
     public:
+        Command();
         Command(std::string const& time, std::string const& userName, Ids id, int tableId = 0);
         std::string getTime();
+        void setTime(std::string const& newTime);
         std::string getUserName();
+        void setUserName(std::string const& newName);
         Ids getOperationId();
+        void setOperationId(Ids newId);
         int getTableId();
+        void setTableId(int newTableId);
+        std::string toString();
+        std::string fromIdToString(Ids id);
 
 
 
--- src/command.cpp
+++ src/command.cpp
@@ -2,18 +2,66 @@
 
 Command::Command(std::string const& time, std::string const& userName, Ids id, int tableId = 0):time(time), userName(userName),id(id),tableId(tableId){}
 
+Command::Command():time(""), userName(""), tableId(0){}
+
 std::string Command::getTime(){
     return time;
+}
+
+void Command::setTime(std::string const& newTime){
+    time = newTime;
 }
 
 std::string Command::getUserName(){
     return userName;
 }
 
+void Command::setUserName(std::string const& newName){
+    userName = newName;
+}
+
 Ids Command::getOperationId(){
     return id;
 }
 
+void Command::setOperationId(Ids newId){
+    id = newId;
+}
+
 int Command::getTableId(){
     return tableId;
+}
+
+void Command::setTableId(int newTableId){
+    tableId = newTableId;
+}
+
+std::string Command::fromIdToString(Ids id){
+    switch (id)
+    {
+    case Ids::COME:
+        return "1";
+    case Ids::SIT:
+        return "2";
+    case Ids::WAIT:
+        return "3";
+    case Ids::LEAVE:
+        return "4";
+    case Ids::LEAVE_AT_THE_AND:
+        return "11";
+    case Ids::SIT_AFTER_FREE:
+        return "12";
+    case Ids::ERROR:
+        return "13";
+    default:
+        return "0";
+    }
+}
+
+std::string Command::toString(){
+    if(!tableId){
+        return time + " " + fromIdToString(id) + " " + userName;
+    }else{
+        return time + " " + fromIdToString(id) + " " + userName + " " + std::to_string(tableId);
+    }
 }

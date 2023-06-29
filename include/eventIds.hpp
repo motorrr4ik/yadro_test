072fbe3fbaf4d4624b0a455377a2e0da9878fe57
motorrr4ik <nikita29042001@yandex.ru>
2023-06-29 15:07:44 +0300

    enum with event ids added

---
+++ include/eventIds.hpp
@@ -0,0 +1,12 @@
+#ifndef EVENTSID
+#define EVENTID
+enum class Ids{
+    COME = 1,
+    SIT = 2,
+    WAIT = 3,
+    LEAVE = 4,
+    LEAVE_AT_THE_AND = 11,
+    SIT_AFTER_FREE = 12,
+    ERROR = 13
+};
+#endif

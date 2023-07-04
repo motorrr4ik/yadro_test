0d1396e77796ffff3efc827764f1b88993441e98
motorrr4ik <nikita29042001@yandex.ru>
2023-07-04 10:59:25 +0300

    file close

--- src/eventHandler.cpp
+++ src/eventHandler.cpp
@@ -57,7 +57,9 @@ void eventHadler::startHandling(){
         }
     }
     if(!flag) return;
+    std::cout << "here" << std::endl;
     if(!workDayStatusFlag) club.endOfWorkDay(club.getEndTime()); //Если клуб не завершает свою работу в процессе обработки файла, завершение просходит после
+    fromFileStream.close();
 }
 
 // Запуск потока обработчика
